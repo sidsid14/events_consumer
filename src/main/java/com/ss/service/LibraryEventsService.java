15f8fc445d18b1655eff1eb47aada6e8c673c815
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-05-09 18:37:52 +0530

    Build service layer to process LibraryEvent - Update Event Type

--- src/main/java/com/ss/service/LibraryEventsService.java
+++ src/main/java/com/ss/service/LibraryEventsService.java
@@ -9,6 +9,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Slf4j
 public class LibraryEventsService {
@@ -27,10 +29,24 @@ public class LibraryEventsService {
                 save(libraryEvent);
                 break;
             case UPDATE:
+                validate(libraryEvent);
+                save(libraryEvent);
                 break;
             default:
                 log.info("Invalid library event type");
         }
+    }
+
+    private void validate(LibraryEvent libraryEvent){
+        if(libraryEvent.getLibraryEventId()==null){
+            throw new IllegalArgumentException("Library event id is missing");
+        }
+
+        Optional<LibraryEvent> libraryEventOptional = libraryEventsRepository.findById(libraryEvent.getLibraryEventId());
+        if(!libraryEventOptional.isPresent()){
+            throw new IllegalArgumentException("Not a valid library event");
+        }
+        log.info("Validation is successful for the library event : {}", libraryEventOptional.get());
     }
 
     private void save(LibraryEvent libraryEvent) {
