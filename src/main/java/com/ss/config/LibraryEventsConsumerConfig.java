62c0f816a98a811b50c684c1c2527087b8f69e77
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-05-10 08:35:27 +0530

    Custom error handler

--- src/main/java/com/ss/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/ss/config/LibraryEventsConsumerConfig.java
@@ -1,9 +1,36 @@
 package com.ss.config;
 
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+
+    //This code is only required if you want to change the default functionality.
+    //For overriding default listener configuration
+    //Available in KafkaAnnotationDrivenConfiguration.class
+
+    public DefaultErrorHandler errorHandler(){
+        //Custom error handler waits for 1 sec and retry 2 times
+        FixedBackOff fixedBackOff = new FixedBackOff(1000L, 2);
+        return new DefaultErrorHandler(fixedBackOff);
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> kafkaConsumerFactory){
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory);
+        factory.setCommonErrorHandler(errorHandler());
+        return factory;
+    }
+
 }
